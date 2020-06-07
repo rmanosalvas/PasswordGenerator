46b64a5d6a616e5ab82f8e575224eab2316fd5c4
Ricardo <manosalvas2013@gmail.com>
2020-06-06 22:02:20 -0400

    message (turning in)

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # 03 JavaScript: Password Generator
 
-Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+In this homework assignment we are tasked to create a 'Password Generator'. The user is asked to check a box if they would like their password to include lowercase letters, uppercase letters, numbers, and/or symbols. The user is also given a slide bar that they can use to determine how long they would like their password to be with a minimum value of 8 and a maximum of 128. When the user has the criteria established, clicking the 'Generate Password' number will appear in the textbox above them. 
 
-If you are unfamiliar with special characters, take a look at [some examples](https://www.owasp.org/index.php/Password_special_characters).
+In this repo we used:
+HTML
+CSS
+Javascript
 
 ## User Story
 
@@ -32,17 +35,9 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 ```
 
-The following image demonstrates the application functionality:
 
-![password generator demo](./Assets/03-javascript-homework-demo.png)
+GitHub Repo:
 
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+https://github.com/rmanosalvas/homework3
 
 - - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Here I grabbed the ID's from my HTML and turned them into variables to use in my functions
 var password = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 var lowercaseBox = document.querySelector("#lowercase");
@@ -7,7 +7,7 @@ var numberBox = document.querySelector("#number");
 var symbolBox = document.querySelector("#symbol");
 var lengthNumber = document.querySelector("#length");
 
-
+// Variable with objects of Generators for Lowercase, Uppercase, Symbols, and Numbers
 var critPass = {
     lower: randomLower(),
     upper: randomUpper(),
@@ -15,6 +15,7 @@ var critPass = {
     special: randomSpecial(),
 };
 
+// Click functions added to the Generate Password button
 generateBtn.addEventListener("click", function (event) {
     event.preventDefault();
     var length = parseInt(lengthNumber.value);
@@ -28,8 +29,11 @@ generateBtn.addEventListener("click", function (event) {
     console.log(length);
 });
 
+// Password creator function, activated inside the Generate Button function
 function passwordCreator(lowercaseChecked, uppercaseChecked, numberChecked, symbolChecked, length) {
 
+
+    // Could not connect the for loop to this variable 
     var createdPassword = "";
 
     console.log(createdPassword);
@@ -49,20 +53,20 @@ function passwordCreator(lowercaseChecked, uppercaseChecked, numberChecked, symb
         symbolChecked
     }].filter(item => Object.values(item)[0]);
 
+    // For Loop to go through the arrays of checked boxes
     for (var i = 0; i < length; i += checkedBox) {
         checkedArray.forEach(type => {
             var finalKey = Object.keys(type)[0];
 
+            createdPassword += critPass[finalKey]();
+
+            // My problem came here, I could not figure out why the critPass[finalKey]() would not call. 
         });
     };
-
-    console.log(createdPassword);
-
-
 };
 
 
-
+// Here is a list of functions that contain the criteria available to the user
 function randomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
@@ -79,15 +83,3 @@ function randomSpecial() {
     var symbols = "!@#$%^&*()~?/";
     return symbols[Math.floor(Math.random() * symbols.length)];
 };
-
-
-// generatePassword 
-// was inside generate btn
-
-
-// Add event listener to generate button
-// Get information from the User
-
-
-// alert("Password needs to be between 8 and 128 characters");
-
